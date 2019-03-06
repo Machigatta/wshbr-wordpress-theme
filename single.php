98603a49593641b9ce7173172e5bb70597fdbbd1
Machigatta <germangameguides@gmail.com>
2019-03-06 16:01:59 +0100

    single page changes

--- single.php
+++ single.php
@@ -8,11 +8,16 @@ $backgroundUrl = "";
 if(have_posts()){
     while(have_posts()){ the_post(); ?>
 	</div>
-	<div style="padding: 10px;border-bottom: 1px solid #ccc;"><h2 style="text-shadow: 1px 1px #c3c3c3;font-family: 'Anton';"><?php the_title();?></h2><div class="post-info">
-           <div class="pub-date"><i class="fa fa-calendar" aria-hidden="true"></i> <?php the_time("j.m.Y");?> | </div>
-            <div><i class="fa fa-user" aria-hidden="true"></i> <?php the_author(); ?> | </div>
-            <div class="blog-tags"><i class="fa fa-tags" aria-hidden="true"></i> <?php the_tags( '<ul class="post-categories"><li>', '</li><li>', '</li></ul>' ); ?></div>
-        </div></div>
+    <div style="padding: 10px;border-bottom: 1px solid #ccc;">
+        <div class="post-info post-info-top">
+            Von <b><?php the_author(); ?></b> - Veröffentlicht am <b><?php the_time("d.m.Y, H:i");?></b>
+        </div>
+        <a class="head_link" href="<?php echo esc_url( get_permalink());?>"><h2 style="text-shadow: 1px 1px #c3c3c3;font-family: 'Anton';"><?php the_title();?></h2></a>
+        <div class="post-info">
+            <div class="blog-tags"><?php the_tags( '<ul class="post-categories"><li>', '</li><li>', '</li></ul>' ); ?></div>
+        </div>
+        
+    </div>
     <div class="col-lg-9 col-md-9 col-xs-12" style="border-right: 1px solid #eee;">
     <div class="blog-entry">
         <div class="blog-content" style="text-align:justify;">
